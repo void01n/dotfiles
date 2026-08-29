f0f62674976619f08ee8b0b68e1518a41c6f94ca
void01 <aetherlowk@proton.me>
2026-08-29 01:45:55 +0000

    Edit launch-v0wwa.sh

--- launch-v0wwa.sh
+++ launch-v0wwa.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-export GI_TYPELIB_PATH="$(find /nix/store -maxdepth 3 -name "Gtk-4.0.typelib" -path "*/home-manager-path/*" 2>/dev/null | head -1 | xargs dirname):${GI_TYPELIB_PATH:-}"
-export LD_LIBRARY_PATH="$(find /nix/store -maxdepth 3 -name "Gtk-4.0.typelib" -path "*/home-manager-path/*" 2>/dev/null | head -1 | xargs dirname | sed 's|/girepository-1.0||'):${LD_LIBRARY_PATH:-}"
+export GI_TYPELIB_PATH="$(find /nix/store -maxdepth 4 -name "Gtk-4.0.typelib" -path "*/home-manager-path/*" 2>/dev/null | head -1 | xargs dirname):${GI_TYPELIB_PATH:-}"
+export LD_LIBRARY_PATH="$(find /nix/store -maxdepth 4 -name "Gtk-4.0.typelib" -path "*/home-manager-path/*" 2>/dev/null | head -1 | xargs dirname | sed 's|/girepository-1.0||'):${LD_LIBRARY_PATH:-}"
 exec python3 ~/v0wwa.py ~/.config/v0wwa/main
